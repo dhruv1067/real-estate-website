171c9fdd771fb40ccdb467423fb7c3a49fbb25ab
dhruv1067 <dhruv67d@gmail.com>
2025-08-06 17:11:55 +0530

    page 6 scrolling motion effect done and some minor styling changes

--- script.js
+++ script.js
@@ -357,3 +357,23 @@ ScrollTrigger.create({
 });
 }
 canvas1()
+
+var clutter = "";
+
+document.querySelector("#page6>h1").textContent.split(" ").forEach(function(dets){
+    clutter += `<span> ${dets} </span>`
+
+    document.querySelector("#page6>h1").innerHTML = clutter;
+})
+
+gsap.to("#page6>h1>span",{
+    scrollTrigger:{
+        trigger:`#page6>h1>span`,
+        start: `top bottom`,
+        end: `bottom top`,
+        scroller: `#main`,
+        scrub: .5
+    },
+    stagger:.2,
+    color:`#fff`
+})
